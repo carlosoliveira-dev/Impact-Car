ba84024b18b72cf90f24eeef68d55958b160e1ce
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-11-24 08:08:47 -0300

    animações adicionadas no menu lateral

--- js/main.js
+++ js/main.js
@@ -9,13 +9,23 @@ const header = document.getElementById('header');
 
 // lógica do menu lateral
 menuIcon.addEventListener('click', () => {
-    menuContent.classList.toggle('hidden');
     header.classList.toggle('hidden');
+    menuContent.classList.toggle('hidden');
+    menuContent.classList.toggle('menu-open');
+    
+    setTimeout(() => {
+        menuContent.classList.remove('menu-open');
+    }, 300);
 });
 
 menuIconClose.addEventListener('click', () => {
-    menuContent.classList.toggle('hidden');
-    header.classList.toggle('hidden');
+    menuContent.classList.toggle('menu-close');
+    setTimeout(() => {
+         header.classList.toggle('hidden');
+         menuContent.classList.toggle('hidden');
+        menuContent.classList.remove('menu-close');
+
+    }, 300);
 });
 
 // lógica do menu de busca
